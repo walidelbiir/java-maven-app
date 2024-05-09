c6eaab3e02ce335f77f8cd06288cd84df9df47a1
unknown <walid.el.biir@gmail.com>
2024-05-09 20:01:11 +0100

    ssyntax

--- jenkins-shared-library/vars/terraform.groovy
+++ jenkins-shared-library/vars/terraform.groovy
@@ -1,6 +1,6 @@
 def call() {
     sh "terraform init"
-    sh "terraform plan -var='BUILD_NUMBER=${env.BUILD_NUMBER}"
+    sh "terraform plan -var='BUILD_NUMBER=${env.BUILD_NUMBER}'"
     sh "terraform apply -auto-approve"
 }
 
