9724c4e1b4da7bb36b3d8835a371f381eb65b432
unknown <walid.el.biir@gmail.com>
2024-05-11 17:07:52 +0100

    creating a VM in azure

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,15 +11,72 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-variable "BUILD_NUMBER" {
-  description = "The build number for the current deployment"
-  type        = string
-  default     = "94" # Default value, can be overridden by the -var flag
+provider "azurerm" {
+  features {}
 }
 
+# variable "BUILD_NUMBER" {
+#   description = "The build number for the current deployment"
+#   type        = string
+#   default     = "94" # Default value, can be overridden by the -var flag
+# }
 
-# Create a container
-resource "docker_container" "java_maven_app" {
-  image = "walidelbir/java_maven_app:${var.BUILD_NUMBER}"
-  name  = "java_maven_app"
+
+# # Create a container
+# resource "docker_container" "java_maven_app" {
+#   image = "walidelbir/java_maven_app:${var.BUILD_NUMBER}"
+#   name  = "java_maven_app"
+# }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "adminpassword"
+  network_interface_ids = [azurerm_network_interface.example.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
 }
